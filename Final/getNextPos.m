270c27904991d50703044184106e8bac9f3f39c1
MrCai <1014305148@qq.com>
2021-12-23 22:56:11 +0800

    :fire: 将 getPossibleNextPolars 合并进 getNextPos

--- Final/getNextPos.m
+++ Final/getNextPos.m
@@ -1,5 +1,5 @@
 function next = getNextPos(ellipsoid_coord, cur, normal_vector)
-    % Get the position the next step should land on.
+    % Get the rectangular coordinate the next step should land on.
     cur_polar = rectToPolar(ellipsoid_coord, cur);
     next_polars = getPossibleNextPolars(cur_polar);
     max_product = 0;
@@ -50,4 +50,21 @@ function polar_coord = rectToPolar(ellipsoid_coord, rect_coord)
     theta = atan(tan_theta);
 
     polar_coord = [theta alpha];
+end
+
+function next_polars = getPossibleNextPolars(cur_polar)
+    % Get possible polar coordinates to land the next step.
+    radius = 0.01;
+    precision = 0.001;
+    cur_theta = cur_polar(1);
+    cur_alpha = cur_polar(2);
+    num = 2 * radius / precision + 1;
+
+    theta_directions = linspace(cur_theta - radius, cur_theta + radius, num);
+    alpha_directions = linspace(cur_alpha - radius, cur_alpha + radius, num);
+    [theta_directions, alpha_directions] = meshgrid(theta_directions, alpha_directions);
+    theta_directions = reshape(theta_directions, [1 num^2]);
+    alpha_directions = reshape(alpha_directions, [1 num^2]);
+
+    next_polars = [theta_directions; alpha_directions];
 end
