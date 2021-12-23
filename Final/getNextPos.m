bf24c9329b6254b6b1f58630a70b57f525de9351
MrCai <1014305148@qq.com>
2021-12-23 23:12:46 +0800

    :art: 优化代码结构

--- Final/getNextPos.m
+++ Final/getNextPos.m
@@ -1,29 +1,30 @@
-function next = getNextPos(ellipsoid_coord, cur, normal_vector)
-    % Get the rectangular coordinate the next step should land on.
-    cur_polar = rectToPolar(ellipsoid_coord, cur);
-    next_polars = getPossibleNextPolars(cur_polar);
+function next_pos = getNextPos(ellipsoid_axes, cur, normal_vector)
+    % Get the rectangular coordinate the next_pos step should land on.
     max_product = 0;
-    next = [0 0 0];
+    next_pos = [0 0 0];
+
+    cur_polar = rectToPolar(ellipsoid_axes, cur);
+    next_polars = getNextPolars(cur_polar);
 
     for next_polar = next_polars
-        next_rect = polarToRect(ellipsoid_coord, next_polar);
+        next_rect = polarToRect(ellipsoid_axes, next_polar);
         next_unit = getUnitVector(cur, next_rect);
         product = next_unit * normal_vector';
 
         if product > max_product
             max_product = product;
-            next = next_rect;
+            next_pos = next_rect;
         end
 
     end
 
 end
 
-function rect_coord = polarToRect(ellipsoid_coord, polar_coord)
+function rect_coord = polarToRect(ellipsoid_axes, polar_coord)
     % Convert polar coordinates to rectangular coordinates.
-    a = ellipsoid_coord(1);
-    b = ellipsoid_coord(2);
-    c = ellipsoid_coord(3);
+    a = ellipsoid_axes(1);
+    b = ellipsoid_axes(2);
+    c = ellipsoid_axes(3);
     theta = polar_coord(1);
     alpha = polar_coord(2);
 
@@ -34,11 +35,11 @@ function rect_coord = polarToRect(ellipsoid_coord, polar_coord)
     rect_coord = [x y z];
 end
 
-function polar_coord = rectToPolar(ellipsoid_coord, rect_coord)
+function polar_coord = rectToPolar(ellipsoid_axes, rect_coord)
     % Convert rectangular coordinates to polar cordinates.
-    a = ellipsoid_coord(1);
-    b = ellipsoid_coord(2);
-    c = ellipsoid_coord(3);
+    a = ellipsoid_axes(1);
+    b = ellipsoid_axes(2);
+    c = ellipsoid_axes(3);
     x = rect_coord(1);
     y = rect_coord(2);
     z = rect_coord(3);
@@ -52,19 +53,17 @@ function polar_coord = rectToPolar(ellipsoid_coord, rect_coord)
     polar_coord = [theta alpha];
 end
 
-function next_polars = getPossibleNextPolars(cur_polar)
-    % Get possible polar coordinates to land the next step.
+function next_polars = getNextPolars(cur_polar)
+    % Get all ends of vectors radiating from `cur_polar`, among which the next_pos step will select one.
     radius = 0.01;
     precision = 0.001;
-    cur_theta = cur_polar(1);
-    cur_alpha = cur_polar(2);
     num = 2 * radius / precision + 1;
 
-    theta_directions = linspace(cur_theta - radius, cur_theta + radius, num);
-    alpha_directions = linspace(cur_alpha - radius, cur_alpha + radius, num);
-    [theta_directions, alpha_directions] = meshgrid(theta_directions, alpha_directions);
-    theta_directions = reshape(theta_directions, [1 num^2]);
-    alpha_directions = reshape(alpha_directions, [1 num^2]);
+    theta = linspace(cur_polar(1) - radius, cur_polar(1) + radius, num);
+    alpha = linspace(cur_polar(2) - radius, cur_polar(2) + radius, num);
+    [theta, alpha] = meshgrid(theta, alpha);
+    theta = reshape(theta, [1 num^2]);
+    alpha = reshape(alpha, [1 num^2]);
 
-    next_polars = [theta_directions; alpha_directions];
+    next_polars = [theta; alpha];
 end
