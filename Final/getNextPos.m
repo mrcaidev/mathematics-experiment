8a62834409875e5a06bc83c9e40384f5b6b98032
MrCai <1014305148@qq.com>
2021-12-23 22:51:35 +0800

    :recycle: 将直角坐标与极坐标的转换函数合并进 getNextPos 文件

--- Final/getNextPos.m
+++ Final/getNextPos.m
@@ -18,3 +18,36 @@ function next = getNextPos(ellipsoid_coord, cur, normal_vector)
     end
 
 end
+
+function rect_coord = polarToRect(ellipsoid_coord, polar_coord)
+    % Convert polar coordinates to rectangular coordinates.
+    a = ellipsoid_coord(1);
+    b = ellipsoid_coord(2);
+    c = ellipsoid_coord(3);
+    theta = polar_coord(1);
+    alpha = polar_coord(2);
+
+    x = a * cos(theta) * cos(alpha);
+    y = b * cos(theta) * sin(alpha);
+    z = c * sin(theta);
+
+    rect_coord = [x y z];
+end
+
+function polar_coord = rectToPolar(ellipsoid_coord, rect_coord)
+    % Convert rectangular coordinates to polar cordinates.
+    a = ellipsoid_coord(1);
+    b = ellipsoid_coord(2);
+    c = ellipsoid_coord(3);
+    x = rect_coord(1);
+    y = rect_coord(2);
+    z = rect_coord(3);
+
+    tan_alpha = (y / b) / (x / a);
+    alpha = atan(tan_alpha);
+
+    tan_theta = sqrt((z^2 / c^2) / (x^2 / a^2 + y^2 / b^2));
+    theta = atan(tan_theta);
+
+    polar_coord = [theta alpha];
+end
